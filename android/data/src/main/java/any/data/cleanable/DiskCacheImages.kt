b10f852d4c5c728e080df313efe979bb10453da2
dokar3 <68095777+dokar3@users.noreply.github.com>
2022-11-26 12:41:33 +0800

    Fix size calculation and cleaning of the `DiskCacheImages`

--- android/data/src/main/java/any/data/cleanable/DiskCacheImages.kt
+++ android/data/src/main/java/any/data/cleanable/DiskCacheImages.kt
@@ -3,20 +3,28 @@ package any.data.cleanable
 import any.base.util.FileUtil
 import any.data.entity.SpaceInfo
 import com.facebook.drawee.backends.pipeline.Fresco
+import com.facebook.imagepipeline.core.ImagePipelineFactory
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import java.io.File
 import kotlin.math.min
 
 class DiskCacheImages : Cleanable {
     override suspend fun clean(): Boolean = withContext(Dispatchers.IO) {
-        Fresco.getImagePipeline().clearDiskCaches()
+        ImagePipelineFactory.getInstance().also {
+            it.mainBufferedDiskCache.clearAll().waitForCompletion()
+            it.smallImageFileCache.clearAll()
+        }
         true
     }
 
     override suspend fun spaceInfo(): SpaceInfo = withContext(Dispatchers.IO) {
         val mainDiskCacheConfig = Fresco.getImagePipeline().config.mainDiskCacheConfig
         val maxSize = mainDiskCacheConfig.defaultSizeLimit
-        val dir = mainDiskCacheConfig.baseDirectoryPathSupplier.get()
+        val dir = File(
+            mainDiskCacheConfig.baseDirectoryPathSupplier.get(),
+            mainDiskCacheConfig.baseDirectoryName,
+        )
         val size = FileUtil.directorySize(dir)
         val available = min(maxSize - size, dir.freeSpace)
         SpaceInfo(size, maxSize, available)
